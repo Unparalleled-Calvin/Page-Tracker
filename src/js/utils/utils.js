88d0aaa1ad67164d67af12dd2acd2a1c28808eba
Chenhao Cui <t-chenhaocui@microsoft.com>
2022-07-29 16:11:02 +0800

    support rendering into an existing element

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -17,12 +17,23 @@ function formatDate(date, format) {
     return format.replace(/yyyy|yy|MM|dd|HH|mm|ss/gi, matched => map[matched])
 }
 
-function renderGraph(id, definition) { // create a div with specific id. render the graph into it.
-    element = document.createElement("div");
-    element.id = id
-    var insertSvg = function(svgCode) {
-        element.innerHTML = svgCode;
-        document.getElementsByTagName("body")[0].append(element)
-    };
-    mermaid.mermaidAPI.render(id, definition, insertSvg);
+function renderGraph(id, definition) { // use or create a div with specific id. render the graph into it.
+    mermaid.mermaidAPI.render(id + "-svg", definition, (svg) => {
+        let element = document.getElementById(id)
+        if (!element) {
+            element = document.createElement("div")
+            element.id = id
+            document.getElementsByTagName("body")[0].append(element)
+        }
+        element.innerHTML = svg
+    })
+}
+
+function getHistoryByDate(date) {
+    let historyKey = "visual-history-" + formatDate(date, "yyyy-MM-dd")
+    let history
+    chrome.storage.sync.get([historyKey], function (result) {
+        history = result.history
+    })
+    return history
 }
