804b12b08a97312b436bf770cc1752d6243c8845
Chenhao Cui <t-chenhaocui@microsoft.com>
2022-08-25 18:19:20 +0800

    feat: real time refresh

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,63 +5,17 @@ addScript("../js/object/Edge.js")
 
 addScript("../js/utils/graph.js")
 
+let displayDate = new Date()
+let containerId = "history"
+let tooltipId = "tooltip"
+let zoom = d3.zoom()
+let refreshInterval = 3000
+
 window.onload = function () {
-    getHistoryByDate(new Date()).then((history) => {
-        history = new History({ history: history })
-        let containerId = "history"
-        let containerSelector = "#" + containerId
-
-        let render = new dagreD3.render();
-
-        let graph = history.graph
-        let g = graph.dagre()
-
-        let svg = d3
-            .select(containerSelector)
-            .attr("width", window.innerWidth)
-            .attr("height", window.innerHeight)
-        let svgGroup = svg.append("g");
-        let zoom = d3.zoom()
-            .on("zoom", function () {
-                svgGroup.attr("transform", d3.event.transform);
-            });
-        svg.call(zoom);
-        render(d3.select(containerSelector + " g"), g);
-
-        let gBox= d3.select(containerSelector + " g").node().getBoundingClientRect()
-        svg.attr("viewBox", "" + (gBox.width - window.innerWidth) / 2 + " " + (gBox.height - window.innerHeight) / 2 + " " + window.innerWidth + " " + window.innerHeight) // show svg in center
-    
-        d3.selectAll(".node").on("mouseenter", function (id) {
-            d3.selectAll(".node").classed("unfocused", true);
-            d3.selectAll(".edgePath").classed("unfocused", true);
-            d3.select(this).classed("focused", true);
-            d3.select(this).classed("unfocused", false);
-
-            d3.select("#tooltip")
-              .html(graph.nodes[id].infoHTML())
-              .style("top", event.pageY + 20 + "px")
-              .style("left", event.pageX + 20 + "px")
-        });
-        d3.selectAll(".node").on("mouseleave", function () {
-            d3.selectAll(".node").classed("unfocused", false);
-            d3.selectAll(".edgePath").classed("unfocused", false);
-            d3.select(this).classed("focused", false);
-
-            d3.select("#tooltip")
-                .style("top", "-500px")
-                .style("left", "-500px")
-        });
-        d3.selectAll(".node").on("mousemove", function () {
-            d3.select("#tooltip")
-              .style("top", event.pageY + 20 + "px")
-              .style("left", event.pageX + 20 + "px")
-        });
-    })
+    readAndRenderGraph(displayDate, containerId, tooltipId, zoom, refreshInterval)
 }
 
 window.onresize = function () {
-    let containerId = "history"
-    let containerSelector = "#" + containerId
     d3.select(containerSelector)
         .attr("width", window.innerWidth)
         .attr("height", window.innerHeight)
--- src/js/utils/graph.js
+++ src/js/utils/graph.js
@@ -13,3 +13,74 @@ function genRandomGraph(n) { // randomly generate a graph with n nodes with at l
     }
     return graph
 }
+
+let previousTransform
+let firstDisplay = true
+
+function readAndRenderGraph(date, containerId, tooltipId, zoom, refreshInterval) {
+    getHistoryByDate(date).then(function (history) {
+        history = new History({ history: history })
+        let containerSelector = "#" + containerId
+        let tooltipSelector = "#" + tooltipId
+
+        let render = new dagreD3.render();
+
+        let graph = history.graph
+        let g = graph.dagre()
+
+        let svg = d3
+            .select(containerSelector)
+            .html("<g></g>")
+            .attr("width", window.innerWidth)
+            .attr("height", window.innerHeight)
+        render(d3.select(containerSelector + " g"), g);
+        let svgGroup = d3.select(containerSelector + " g")
+        zoom.on("zoom", function () {
+            previousTransform = d3.event.transform
+            svgGroup.attr("transform", d3.event.transform);
+        });
+        svg.call(zoom.transform, (previousTransform ? d3.zoomIdentity
+            .scale(previousTransform.k)
+            .translate(
+                previousTransform.x / previousTransform.k,
+                previousTransform.y / previousTransform.k
+            ) : d3.zoomIdentity));
+        svg.call(zoom);
+        
+        if (firstDisplay) {
+            firstDisplay = false
+            let gBox = d3.select(containerSelector + " g").node().getBoundingClientRect()
+            svg.attr("viewBox", "" + (gBox.width - window.innerWidth) / 2 + " " + (gBox.height - window.innerHeight) / 2 + " " + window.innerWidth + " " + window.innerHeight) // show svg in center
+        }
+
+        d3.selectAll(".node").on("mouseenter", function (id) {
+            d3.selectAll(".node").classed("unfocused", true);
+            d3.selectAll(".edgePath").classed("unfocused", true);
+            d3.select(this).classed("focused", true);
+            d3.select(this).classed("unfocused", false);
+
+            d3.select(tooltipSelector)
+                .html(graph.nodes[id].infoHTML())
+                .style("top", event.pageY + 20 + "px")
+                .style("left", event.pageX + 20 + "px")
+        });
+        d3.selectAll(".node").on("mouseleave", function () {
+            d3.selectAll(".node").classed("unfocused", false);
+            d3.selectAll(".edgePath").classed("unfocused", false);
+            d3.select(this).classed("focused", false);
+
+            d3.select(tooltipSelector)
+                .style("top", "-500px")
+                .style("left", "-500px")
+        });
+        d3.selectAll(".node").on("mousemove", function () {
+            d3.select(tooltipSelector)
+                .style("top", event.pageY + 20 + "px")
+                .style("left", event.pageX + 20 + "px")
+        });
+
+        setTimeout(function () {
+            readAndRenderGraph(date, containerId, tooltipId, zoom, refreshInterval)
+        }, refreshInterval);
+    })
+}
