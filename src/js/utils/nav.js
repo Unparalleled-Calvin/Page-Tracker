b4040edc23c09cee01832f9cc7cb2772b1051a7f
Calvin <1176479962@qq.com>
2022-09-03 21:20:08 +0800

    move calendar to button

--- src/js/utils/nav.js
+++ src/js/utils/nav.js
@@ -10,13 +10,13 @@ function unfoldSideBar() {
     d3.select(".content").style("left", "25%");
     d3.select(".content").style("width", "75%");
     d3.select(".toggle-button").classed("active", true);
-} 
+}
 
 d3.select(".toggle-button").on("click", function () {
-    if(d3.select(this).classed("active")) {
+    if (d3.select(this).classed("active")) {
         foldSideBar()
     }
-    else{
+    else {
         unfoldSideBar()
     }
 })
@@ -24,12 +24,33 @@ d3.select(".toggle-button").on("click", function () {
 d3.select(".clear-button").on("click", function () {
     let to_delete = confirm("Clear data in " + formatDate(date, "yyyy-MM-dd") + "?");
     if (to_delete) {
-        chrome.runtime.sendMessage({name: "delete", date: formatDate(date, "yyyy-MM-dd")}, function(response) {
-            if(response.status != "ok") {
+        chrome.runtime.sendMessage({ name: "delete", date: formatDate(date, "yyyy-MM-dd") }, function (response) {
+            if (response.status != "ok") {
                 alert("Failed!")
             }
         });
     }
+})
+
+function unfoldCalendar() {
+    d3.select(".calendar").style("opacity", "100%").style("z-index", "1")
+}
+
+function foldCalendar() {
+    d3.select(".calendar").style("opacity", "0%").style("z-index", "-1")
+}
+
+d3.select(".calendar-button").on("click", function () {
+    if (d3.select(".calendar").style("opacity") == "0") {
+        unfoldCalendar()
+    }
+    else {
+        foldCalendar()
+    }
+})
+
+d3.select(".calendar").on("mouseleave", function () {
+    foldCalendar()
 })
 
 function adjustWindow() {
